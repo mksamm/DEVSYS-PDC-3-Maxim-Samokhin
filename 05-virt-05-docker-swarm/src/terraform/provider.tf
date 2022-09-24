01149360b2ba12f040d3ee4d1744f13e8caf236d
Maxim Samokhin <78801043+mksamm@users.noreply.github.com>
2022-09-24 10:56:45 +0300

    Update provider.tf

--- 05-virt-05-docker-swarm/src/terraform/provider.tf
+++ 05-virt-05-docker-swarm/src/terraform/provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = "y0_AgAAAAAj9_U2AATuwQAAAADPYgt1MS8QZD5DSA6f2Niyy7u4n5oaDLc"
+  tokem = "y0_AgAAAAAj9_U2AATuwQAAAADPYgt1MS8QZD5DSA6f2Niyy7u4n5oaDLc"
   cloud_id  = "${var.yandex_cloud_id}"
   folder_id = "${var.yandex_folder_id}"
 }
