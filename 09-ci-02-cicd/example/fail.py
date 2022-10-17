ce811a4e84c0d4c2911ec21847401efcdeeb49b3
Maxim Samokhin <78801043+mksamm@users.noreply.github.com>
2022-10-17 16:38:53 +0300

    Update fail.py

--- 09-ci-02-cicd/example/fail.py
+++ 09-ci-02-cicd/example/fail.py
@@ -1,12 +1,10 @@
-def increment(index):
-    index =+ 1
-    return index
+def increment(index): 
+    res = index + 1 
+    return res
 def get_square(numb):
     return numb*numb
 def print_numb(numb):
     print("Number is {}".format(numb))
-    pass
-
 index = 0
 while (index < 10):
     index = increment(index)
