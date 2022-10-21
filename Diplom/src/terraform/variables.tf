60c8372c0c924b040a54999d34da237b6c4e1694
Maxim Samokhin <78801043+mksamm@users.noreply.github.com>
2022-10-21 20:21:41 +0300

    Update variables.tf

--- Diplom/src/terraform/variables.tf
+++ Diplom/src/terraform/variables.tf
@@ -7,17 +7,17 @@ variable "token" {
 #ID облака
 variable "cloud_id" {
   type    = string
-  default = ""
+  default = "b1gb8tnrcg5uk53e5b74"
 }
 
 #ID каталога
 variable "folder_id" {
   type    = string
-  default = ""
+  default = "b1go89oq3fbg6nene4rm"
 }
 #Белый IP
 variable "yc_dedicated_ip" {
-  default = ""
+  default = "194.58.112.174"
 }
 #Доменное имя
 variable "domain_name" {
